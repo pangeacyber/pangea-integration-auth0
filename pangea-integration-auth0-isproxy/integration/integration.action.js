3a770d21b8768f0a494195c06798d84fbe544e21
North McCormick <north.mccormick@pangea.cloud>
2025-05-05 10:59:14 -0700

    PAN-20075: Handle IPv4, fix proxy score check

--- pangea-integration-auth0-isproxy/integration/integration.action.js
+++ pangea-integration-auth0-isproxy/integration/integration.action.js
@@ -1,11 +1,14 @@
 exports.onExecutePostLogin = async (event, api) => {
     const Pangea = require('pangea-node-sdk');
     const token = event.secrets.TOKEN;
-    const domain = event.secrets.DOMAIN;
-    const intelprovider = event.secrets.PROVIDER;
+    const domain = event?.configuration?.DOMAIN ? event.configuration.DOMAIN : event?.secrets?.DOMAIN;
+    const intelprovider = event?.configuration?.PROVIDER ? event.configuration.PROVIDER : event?.secrets?.PROVIDER;
     const config = new Pangea.PangeaConfig({domain: domain});
     const audit = new Pangea.AuditService(token, config);
     const ipIntel = new Pangea.IPIntelService(token, config);
+
+    const ipv4Test = /^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$/
+
 
     const ip = event.request.ip;
     const options = {provider: intelprovider, verbose: true, raw: true};
@@ -23,27 +26,29 @@ exports.onExecutePostLogin = async (event, api) => {
         "source": ip
     };
 
-    let ip_response;
-    try {
-        //console.log("Checking IP Reputation: '%s'", ip);
-        ip_response = await ipIntel.isProxy(ip, options);
-        data.new['ip_response'] = ip_response.gotResponse.body;
-        //console.log("Response: ", ip_response.gotResponse.body);
-    } catch (error) {
-        ip_response = {"status": "Failed", "summary": error};
-    }
+    if (ipv4Test.test(ip)) {
+        let ip_response;
 
+        try {
+            ip_response = await ipIntel.isProxy(ip, options);
 
-    if (ip_response.status == "Success" && ip_response.result.data.score < 70) {
-        data["status"] = "Success";
-        data["message"] = "Passed Proxy Check";
+            data.new['ip_response'] = ip_response.gotResponse.body;
+        } catch (error) {
+            ip_response = {"status": "Failed", "summary": error};
+        }
+
+        if (ip_response.status == "Success" && ip_response.result.data.is_proxy === false) {
+            data["status"] = "Success";
+            data["message"] = "Passed Proxy Check";
+        } else {
+            api.access.deny('proxy_check_failed', "Login Failed");
+            data["status"] = "Failed";
+            data["message"] = "Failed Proxy Check - " + ip_response.summary;
+        }
     } else {
-        api.access.deny('proxy_check_failed', "Login Failed");
-        data["status"] = "Failed";
-        data["message"] = "Failed Proxy Check - " + ip_response.summary;
+        data["status"] = "Success";
+        data["message"] = "Skipped proxy check, IP not valid IPv4";
     }
 
-    console.log("Pangea Execution Data: ", data);
-    //const logResponse = await audit.log(data);
-    //console.log("Data: ", logResponse)
+    await audit.log(data);
 };
